2f043d4542951c42238d581acc93f5490cd0a355
linguistikadotorg <75524525+linguistikadotorg@users.noreply.github.com>
2023-04-25 23:05:10 -0300

    a

---
+++ a.js
@@ -0,0 +1,1 @@
+var _paq = window._paq = window._paq || []; _paq.push(['trackPageView']); _paq.push(['enableLinkTracking']); (function() { var u="//data.linguistika.org/"; _paq.push(['setTrackerUrl', u+'a.php']); _paq.push(['setSiteId', '3']); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.async=true; g.src=u+'a.js'; s.parentNode.insertBefore(g,s); })();
